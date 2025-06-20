579fd69df02cea5ac566dc777138601ec4595aab
Maksalina95 <maksalinamagazin@gmail.com>
2025-06-20 18:56:42 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,14 +11,21 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // ⚠ Не кэшируем внешние запросы (например, таблицу или фото из i.ibb.co)
+  if (url.origin !== location.origin) {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
