28a05d0028e7305627d713f9c66ae3dda6c884c7
Maksalina95 <maksalinamagazin@gmail.com>
2025-06-27 17:35:48 +0300

    Update favorites.js
    o

--- favorites.js
+++ favorites.js
@@ -1,5 +1,5 @@
 const sheetId = '1gBcuPzWv_nH2i7sWyCaERVCjO-hLg8EcndPkEMlNqgw';
-const url = https://opensheet.elk.sh/${sheetId}/Sheet1;
+const url = `https://opensheet.elk.sh/${sheetId}/Sheet1`;
 
 const favoritesList = document.getElementById('favorites-list');
 const favoriteIds = JSON.parse(localStorage.getItem('favorites') || '[]');
