df5d29aee08ae51ba9e4f165d384ccd01ba98e52
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-02 00:26:19 +0300

    Update filter-search.js
    обнова

--- filter-search.js
+++ filter-search.js
@@ -1,4 +1,4 @@
-const urlFilter = `${baseUrl}/Sheet1`;
+const filterSearchUrl = `${baseUrl}/Sheet1`;
 
 const productList = document.getElementById('product-list');
 const searchInput = document.getElementById('searchInput');
