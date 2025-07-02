4468fcf08d5230d1948c27bcffc6445a3132834a
Maksalina95 <maksalinamagazin@gmail.com>
2025-07-02 07:31:02 +0300

    Update filter-search.js
    o

--- filter-search.js
+++ filter-search.js
@@ -33,7 +33,7 @@ function showProducts(list) {
         ? `<video controls src="${item.видео}"></video>` 
         : `<img src="${item.фото}" alt="${item.название}" />`}
       <h3>${item.название}</h3>
-      ${item.описание ? <p>${item.описание}</p> : ''}
+      ${item.описание ? `<p>${item.описание}</p>` : ''}
       <strong>${item.цена} ₽</strong>
       <div class="card-buttons">
         <a href="https://wa.me/79376280080" target="_blank">WhatsApp</a>
